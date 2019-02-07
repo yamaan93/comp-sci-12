08cf78ea825ad1291643b38f4d04e7ff64e5b114
yamaan93 <yamaan93@gmail.com>
2019-02-07 10:54:27 -0500

    started i guess

---
+++ Calculator/src/Calculator.cpp
@@ -0,0 +1,29 @@
+//============================================================================
+// Name        : Calculator.cpp
+// Author      : Yamaan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+int add();
+int subtract();
+int divide();
+int mmultiply();
+int main() {
+	int user_selection;
+	int []input_values = {};
+	cout << "Calculator Console Application" << endl;
+	cout << " would you like to :"<<endl;
+	cout << " 1. add"<<endl;
+	cout << " 2. substract"<<endl;
+	cout << " 3. divide"<<endl;
+	cout << " 4. multiply"<<endl;
+
+	cin>> user_selection;// intake what operation the user would like to partake in
+	cout
+
+	return 0;
+}
