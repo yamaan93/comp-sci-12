9f42993465a18e1a0efcae43415400f25f6c2dba
yamaan93 <yamaan93@gmail.com>
2019-02-12 14:06:05 -0500

    doesnt work anymore

--- Calculator/src/Calculator.cpp
+++ Calculator/src/Calculator.cpp
@@ -12,55 +12,81 @@ int add(int x, int y);
 int subtract(int x, int y);
 int divide(int x, int y);
 int multiply(int x, int y);
-int average(int averge[]);
+int average(int averge[], int size);
 int main() {
-	int user_selection;
-	int input_values[2] = {};
-	cout << "Calculator Console Application" << endl;
-	cout << " would you like to :"<<endl;
-	cout << " 1. add"<<endl;
-	cout << " 2. subtract"<<endl;
-	cout << " 3. divide"<<endl;
-	cout << " 4. multiply"<<endl;
+	while (true) {
+		int user_selection;
+		int input_values[2] = { };
+		cout << "Calculator Console Application" << endl;
+		cout << " would you like to :" << endl;
+		cout << " 1. add" << endl;
+		cout << " 2. subtract" << endl;
+		cout << " 3. divide" << endl;
+		cout << " 4. multiply" << endl;
 
-	cin>> user_selection;// intake what operation the user would like to partake in
-	cout<<user_selection<<endl;
-	cout << "what 2 numbers would you like to use"<<endl;
-	cin >> input_values[0];
-	cout <<"what is the second number?"<<endl;
-	cin >> input_values[1];
-	cout<<user_selection<<endl;
-	if(user_selection == 1){
+		cin >> user_selection; // intake what operation the user would like to partake in
+		cout << user_selection << endl;
+		if(user_selection ==213131332){
+		cout << "what 2 numbers would you like to use" << endl;
+		cin >> input_values[0];
+		cout << "what is the second number?" << endl;
+		cin >> input_values[1];
+		cout << user_selection << endl;
+		}
+		if (user_selection == 1) {
 
-		add(input_values[0],input_values[1]);
+			add(input_values[0], input_values[1]);
 
+		}
+		if (user_selection == 2) {
+			subtract(input_values[0], input_values[1]);
+		}
+		if (user_selection == 3) {
+			divide(input_values[0], input_values[1]);
+		}
+		if (user_selection == 4) {
+			multiply(input_values[0], input_values[1]);
+		}
+		if (user_selection == 5) {
+			int size;
+			cout << "how many values do you want" << endl;
+			cin >> size;
+			int average[size];
+			for (int i = 0; i < size + 1; i++) {
+				printf("value %d :", i);
+				cin >> average[i];
+			}
+			average(average[].size);
+
+		}
 	}
-	if(user_selection == 2){
-		subtract(input_values[0],input_values[1]);
-	}
-
 
 	return 0;
 }
-int add(int x, int y){
-	int result = x+y;
-	printf("%d+%d=%d",x,y,result);
+int add(int x, int y) {
+	int result = x + y;
+	printf("%d+%d=%d \n", x, y, result);
 	return result;
 }
-int subtract(int x, int y){
-	int result = x-y;
-	printf("%d-%d=%d",x,y,result);
+int subtract(int x, int y) {
+	int result = x - y;
+	printf("%d-%d=%d \n", x, y, result);
 	return result;
 }
-int divide(int x, int y){
-	int result = x/y;
+int divide(int x, int y) {
+	int result = x / y;
+	printf("%d/%d=%d \n", x, y, result);
 	return result;
 }
-int multiply(int x, int y){
-	int result = x*y;
+int multiply(int x, int y) {
+	int result = x * y;
+	printf("%dX%d=%d \n", x, y, result);
 	return result;
 }
-int avergage(int values[]){
+int avergage(int values[], int size) {
 	int result;
+	for (int i = 0; i < size; i++) {
+
+	}
 	return result;
 }
