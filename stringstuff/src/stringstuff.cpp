0d0a675da18fd0ca01316c79b6f7cb2b2da70f7a
yamaan93 <yamaan93@gmail.com>
2019-02-28 10:51:00 -0500

    new string practice boi

---
+++ stringstuff/src/stringstuff.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : stringstuff.cpp
+// Author      : Yamaan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+#include <string.h>
+
+int main() {
+
+	return 0;
+}
