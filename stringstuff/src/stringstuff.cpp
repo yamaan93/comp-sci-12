13954dfb9cac79fa2b7020463a6a11cd67b22ef2
yamaan <yamaan93@gmail.com>
2019-03-25 09:41:14 -0400

    reads in line by line.

--- stringstuff/src/stringstuff.cpp
+++ stringstuff/src/stringstuff.cpp
@@ -1,16 +1,53 @@
-//============================================================================
-// Name        : stringstuff.cpp
-// Author      : Yamaan
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
 
+// Reading into an array of chars using fscanf()
+
+#include <stdio.h>
 #include <iostream>
 using namespace std;
-#include <string.h>
-
+#define FILENAME "C:\\Users\\Yamaan\\Documents\\essay_output.txt"
 int main() {
 
-	return 0;
+   FILE *fPtr;
+   FILE *fPtr1;
+   // fopen() returns NULL on failure
+   if ((fPtr = fopen("C:\\Users\\Yamaan\\Documents\\test.txt", "r+")) == NULL) {
+      printf("Error opening file\n");
+      return 0;
+   }
+   if ((fPtr1 = fopen(FILENAME, "w")) == NULL) {
+	   printf("Error opening file\n");
+	        return 1;
+   }
+   char a[300][30];
+
+   // fscanf() returns the number of items successfully read
+   // The field widths in the format specifier need to be one less
+   // than the size of the arrays, in order to leave space for the
+   // null terminator.
+ /*  if (fscanf(fPtr, "%9s %9s", a[0],a[1]) != 2) {
+      printf("Error reading file\n");
+      return 0;
+   }
+
+   */
+   int ticks=0;
+   for(int i=0;!feof(fPtr); i++) {
+
+       // fscanf() returns the number of items successfully read
+       if ((fscanf(fPtr, "%9s", a[i]) != 1) && (!feof(fPtr))) {
+          printf("Error reading file\n");
+          return 0;
+       }
+
+       ticks++;
+      cout<<a[i]<<endl;
+    }
+
+
+   // fclose() returns 0 on successful completion
+   if (fclose(fPtr) != 0) {
+      printf("Error closing file\n");
+      return 0;
+   }
+
 }
