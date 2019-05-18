9bfd768e42fc54ffed47cbffdc74557dc328ca21
yamaan93 <yamaan93@gmail.com>
2019-05-18 03:05:31 -0400

    hi

---
+++ ISP/src/ISP.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : ISP.cpp
+// Author      : Yamaan Bakir
+// Version     :
+// Copyright   : dont copy plz
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string.h>
+#include <fstream>
+using namespace std;
+void saveCSV(string save){
+	ofstream myfile;
+	myfile.open("test.csv");
+
+}
+int main() {
+
+	return 0;
+}
