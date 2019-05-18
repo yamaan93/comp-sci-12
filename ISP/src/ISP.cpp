7e2cfbc0b284b565a9b6c01b656be4fb5c8e9000
yamaan93 <yamaan93@gmail.com>
2019-05-18 03:24:57 -0400

    prepped read and write CSV, write solid, read broken

--- ISP/src/ISP.cpp
+++ ISP/src/ISP.cpp
@@ -10,12 +10,32 @@
 #include <string.h>
 #include <fstream>
 using namespace std;
+string readCSV(){
+	string in;
+	int count = 0;
+	string line;
+	ifstream myfile;
+	myfile.open("test.csv");
+	while(myfile.good()){
+		count ++;
+		getline(myfile, line, ',');
+	}
+
+	myfile.close();
+	return in;
+}
 void saveCSV(string save){
 	ofstream myfile;
 	myfile.open("test.csv");
+	for(int i = 0; i<save.length(); i++){
+		myfile<<save[i]<<",";
+	}
+	myfile.close();
 
 }
 int main() {
-
+	string test = "ha noith u";
+	test =readCSV();
+	cout<<test<<endl;
 	return 0;
 }
