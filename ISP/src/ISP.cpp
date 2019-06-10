e8ae736eb52d0068bfef1fcc27d4d819ac70b29a
yamaan93 <yamaan93@gmail.com>
2019-06-09 22:25:28 -0400

    im too tired to write main rn

--- ISP/src/ISP.cpp
+++ ISP/src/ISP.cpp
@@ -14,12 +14,19 @@
 // fix all your problems using the built in new class button cause c++ is hot garbage
 
 using namespace std;
-
+Book library[5];
+int library_count=0;
 int main() {
-	Book paper("book uno");
-	paper.readCSV();
-	paper.delete_person(1);
-	paper.saveCSV();
-	paper.print();
+
+	while(true){
+		cout<<"welcome to address book++"<<endl;
+		if(library_count>0){
+			cout<<"here are your books"<<endl;
+			for(int i =0; i <library_count; i++){
+				cout<<i<<": "<<library[i].bookname<<endl;
+			}
+		}
+		cout<<"what would you like to do? /n"<<endl;
+	}
 	return 0;
 }
