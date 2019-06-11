ba5acc37f74665047d11fcfefb143c521c82677c
yamaan <yamaan93@gmail.com>
2019-06-11 10:31:33 -0400

    add and delete book made, main is ready

--- ISP/src/ISP.cpp
+++ ISP/src/ISP.cpp
@@ -14,19 +14,64 @@
 // fix all your problems using the built in new class button cause c++ is hot garbage
 
 using namespace std;
-Book library[5];
+Book *library[5];
 int library_count=0;
-int main() {
 
+void new_book(string name);
+void open_book(int selection);
+void delete_book(int selection);
+void list_books(){
+	for(int i =0; i <library_count; i++){
+					cout<<i<<": "<<library[i]->bookname<<endl;
+				}
+}
+int main() {
+	library[0] = new Book("big");
+	library_count++;
 	while(true){
+		int input=0;
 		cout<<"welcome to address book++"<<endl;
 		if(library_count>0){
 			cout<<"here are your books"<<endl;
-			for(int i =0; i <library_count; i++){
-				cout<<i<<": "<<library[i].bookname<<endl;
-			}
+			list_books();
 		}
-		cout<<"what would you like to do? /n"<<endl;
+		else
+			cout<< "you have no books to show! its empty in here, just like your heart \n \n"<<endl;
+		cout<<"what would you like to do? \n"<<endl;
+		cout<<"1. create new book \n 2. open book \n 3. delete book \n 4. merge books \n"<<endl;
+		cin>>input;
+		if (input == 1){
+			cout<<" please name the new book: "<<endl;
+			string name = "";
+			cin>>name;
+			new_book(name);
+		}
+		if(input== 2){
+			list_books();
+			cout<<"please enter the number of the book you would like to open"<<endl;
+			int selecter= 10000;
+			cin>>selecter;
+			open_book(selecter);
+		}
+		if(input == 3){
+			list_books();
+			cout<<"please enter the number of the book you would like to delete"<<endl;
+			int selecter= 10000;
+			cin>>selecter;
+			delete_book(selecter);
+		}
+		if(input ==4){
+			// put the dumb book + book;
+		}
 	}
 	return 0;
+}
+void new_book(string name){
+
+}
+void open_book(int selection){
+
+}
+void delete_book(int selection){
+
 }
